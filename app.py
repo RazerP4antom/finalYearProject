1b1b61d9bc7b6d2b85b3044591dd4bdb79cd975d
RazerP4antom <mirjisarthak3@gmail.com>
2023-03-05 19:34:47 +0530

    update

--- app.py
+++ app.py
@@ -74,6 +74,72 @@ def buy_sell_hold(stock_symbol):
         
     return pie
 
+
+def research_report_icic(stock_symbol):
+    # base URL
+    base_url = "https://www.icicidirect.com/stocks/"
+    company_extension = stock_symbol.lower().replace(" ", "-") + "-share-price" 
+    full_url = base_url + company_extension
+    response = requests.get(full_url)
+    html_content = response.text
+
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    div = soup.find("div", id="ResearchReportTab")
+    if div:
+        a_tag = div.find("a", text="Click here for full recommendation")
+        full_recommendation_link = a_tag.get("href")
+        # print(full_recommendation_link)
+        response1 = requests.get(full_recommendation_link)
+        html_content1 = response1.text
+
+        soup = BeautifulSoup(html_content1, "html.parser")
+
+        recommendation = None
+        for div in soup.find_all("div", class_="box bg_grey mb-3"):
+            for strong in div.find_all("strong"):
+                if "What should Investors do?" in strong.text:
+                    recommendation = div.text
+        
+        if recommendation:
+            return(recommendation)
+        else:
+            return"Recommendation not found"
+        
+    else:
+        return"Not fully researched by ICICI direct"
+    
+def research_report_ecotimes(stock_symbol):
+    first_letter = stock_symbol[0]
+    company_extension = stock_symbol.lower().replace(" ", "-")
+    url = "https://economictimes.indiatimes.com/markets/stocks/stock-quotes/" + first_letter
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    company_link = None
+    for ul in soup.find_all('ul', class_='companyList'):
+        for li in ul.find_all('li'):
+            a_tag = li.find('a')
+            if a_tag is not None:
+                url_term = a_tag.get('href').split('/')[-3]
+                if company_extension in url_term:
+                    company_link = a_tag.get('href')
+                    break
+        if company_link is not None:
+            full_url = "https://economictimes.indiatimes.com" + company_link
+            break
+
+    if full_url is None:
+        return"Company not Found"
+    else:
+        response = requests.get(full_url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        recommendation = soup.find('div', {'class': 'meanTxt'}).span.text
+        if recommendation is None:
+            return"No recommendation found"
+        else:
+            return"Recommendation as per analysts: {}".format(recommendation) 
+
 def historical_closing_price(ticker):
     today = datetime.date.today()
     stock_price = nse.get_history(symbol = ticker, start=date(2022,1,1), end=today)
@@ -113,73 +179,6 @@ def ratios(ticker):
         ratios.append(f'{name}: {value}')
 
     return ratios
-
-
-def research_report_ecotimes(stock_symbol):
-    first_letter = stock_symbol[0]
-    company_extension = stock_symbol.lower().replace(" ", "-")
-    url = "https://economictimes.indiatimes.com/markets/stocks/stock-quotes/" + first_letter
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    company_link = None
-    for ul in soup.find_all('ul', class_='companyList'):
-        for li in ul.find_all('li'):
-            a_tag = li.find('a')
-            if a_tag is not None:
-                url_term = a_tag.get('href').split('/')[-3]
-                if company_extension in url_term:
-                    company_link = a_tag.get('href')
-                    break
-        if company_link is not None:
-            full_url = "https://economictimes.indiatimes.com" + company_link
-            break
-
-    if full_url is None:
-        return"Company not Found"
-    else:
-        response = requests.get(full_url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        recommendation = soup.find('div', {'class': 'meanTxt'}).span.text
-        if recommendation is None:
-            return"No recommendation found"
-        else:
-            return"Recommendation as per analysts: {}".format(recommendation) 
-
-
-def research_report_icic(stock_symbol):
-    # base URL
-    base_url = "https://www.icicidirect.com/stocks/"
-    company_extension = stock_symbol.lower().replace(" ", "-") + "-share-price" 
-    full_url = base_url + company_extension
-    response = requests.get(full_url)
-    html_content = response.text
-
-    soup = BeautifulSoup(html_content, "html.parser")
-
-    div = soup.find("div", id="ResearchReportTab")
-    if div:
-        a_tag = div.find("a", text="Click here for full recommendation")
-        full_recommendation_link = a_tag.get("href")
-        # print(full_recommendation_link)
-        response1 = requests.get(full_recommendation_link)
-        html_content1 = response1.text
-
-        soup = BeautifulSoup(html_content1, "html.parser")
-
-        recommendation = None
-        for div in soup.find_all("div", class_="box bg_grey mb-3"):
-            for strong in div.find_all("strong"):
-                if "What should Investors do?" in strong.text:
-                    recommendation = div.text
-        
-        if recommendation:
-            return(recommendation)
-        else:
-            return"Recommendation not found"
-        
-    else:
-        return"Not fully researched by ICICI direct"
     
 
 
